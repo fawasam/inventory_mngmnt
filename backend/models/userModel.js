aa82a882d64cbde3a1c5268b8ee13fcab0aab183
fawasam <fawasam5@gmail.com>
2022-11-15 18:54:03 +0530

    user model and route for register

---
+++ backend/models/userModel.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      trim: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      select: false,
+      required: [true, "Please add a password"],
+      minlength: [6, "Password must be at least 6 characters"],
+      // maxlength: [23, "Password must not be more than 23 characters"],
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    photo: {
+      type: String,
+      required: [true, "Please add a photo "],
+      default:
+        "https://www.kindpng.com/picc/m/451-4517876_default-profile-hd-png-download.png",
+    },
+    phone: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      maxlength: [250, "Bio must not be more than 250 characters"],
+      default: "add some bio",
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "superAdmin"],
+      default: "user",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+//  Encrypt password before saving to db
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model("User", UserSchema);
--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleware/errorMiddleware");
 
-//middleware
-
-//routes
-
-//app use
+//02:05
 
 //connect to database
 connectDB();
 
 const app = express();
+//app use
+app.use(errorHandler);
+
+//middleware
+app.use(express.json());
+app.unsubscribe(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//routes
+app.use("/api/v1/auth", userRoute);
+
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
 
 const PORT = process.env.PORT || 5000;
 
